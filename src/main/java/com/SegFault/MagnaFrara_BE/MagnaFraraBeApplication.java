88999739e6a0e3d5fb7844a734872aced5e0fa3a
Emanuele Fava <nicofava68@gmail.com>
2025-08-16 17:00:32 +0200

    Secondo Test

---
+++ src/main/java/com/SegFault/MagnaFrara_BE/MagnaFraraBeApplication.java
@@ -0,0 +1,13 @@
+package com.SegFault.MagnaFrara_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagnaFraraBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagnaFraraBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SegFault/MagnaFrara_BE/MagnaFraraBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SegFault.MagnaFrara_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagnaFraraBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
