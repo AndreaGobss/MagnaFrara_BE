2e96042ffc4ea4edf0d0e487c587241243f6d8c8
Emanuele Fava <nicofava68@gmail.com>
2025-08-16 17:30:34 +0200

    Inizializzato progetto. Configurate build e properties

--- src/main/java/com/SegFault/MagnaFrara_BE/MagnaFraraBeApplication.java
+++ src/main/java/com/SegFault/MagnaFrara_BE/MagnaFraraBeApplication.java
@@ -2,8 +2,12 @@ package com.SegFault.MagnaFrara_BE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableTransactionManagement
 public class MagnaFraraBeApplication {
 
 	public static void main(String[] args) {
