926b12990cf8980fd6dc83db012636fe24f98fb8
AndreaGobss <andrea01.gobbi@edu.unife.it>
2025-09-16 11:05:12 +0200

    Aggiustato errore cors

--- src/main/java/com/SegFault/MagnaFrara_BE/config/CorsConfig.java
+++ src/main/java/com/SegFault/MagnaFrara_BE/config/CorsConfig.java
@@ -15,7 +15,8 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                    "http://localhost:4200",
+                    "http://localhost:4200", 
+                    "http://127.0.0.1:4200",
                     "http://0.0.0.0:4200"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
